3fbf7bf3c3b7213ea8db8642b6c5e24164f0234d
@risto <aristohouessou@gmail.com>
2020-10-16 17:30:40 +0200

    Ajout FRaction

---
+++ Fraction.java
@@ -0,0 +1,13 @@
+public class Fraction{
+    private int numerateur;
+    private int denominateur;
+
+    public Fraction(int numerateur, int denominateur) {
+        this.denominateur=denominateur;
+        this.numerateur=numerateur;
+    }
+    public String toString() {
+        return "numerateur:"+this.numerateur+"   denominateur:"+this.denominateur;
+    }
+    
+}
